6f45e4d5fb15d3150e1ff94f630317469d7ccc55
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-06-07 13:08:14 +0300

    LittleTouches

--- script.js
+++ script.js
@@ -5,9 +5,31 @@ $(document).ready(function() {
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0});
+    });
+   
     $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active')
     });
+    // Text animations scripts
+    var typed = new Typed(".text-animation",{
+        strings:["Developer","Designer","Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+    var typed = new Typed(".text-animation-2",{
+        strings:["Developer","Designer","Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
 });
